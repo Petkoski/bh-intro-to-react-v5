dba30cdb3901bb3336666ab56713f4649a386be4
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2020-06-19 19:32:12 +0200

    -Convert project to JSX, add React-specific ESLint config

--- src/Pet.js
+++ src/Pet.js
@@ -2,13 +2,21 @@ import React from "react";
 
 export default function Pet({ name, animal, breed }) {
   //Destructuring
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("div", {}, [
-      React.createElement("h1", {}, name),
-      React.createElement("h2", {}, animal),
-      React.createElement("h2", {}, breed),
-    ])
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, animal),
+  //     React.createElement("h2", {}, breed),
+  //   ])
+  // );
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
   );
 }
